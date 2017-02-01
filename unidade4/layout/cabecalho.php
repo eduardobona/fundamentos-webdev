c699051d11378ae1668bab84d77685f08e3ca7a1
Eduardo Bona <eduardobona@MacBook-Pro-de-Eduardo.local>
2017-02-01 12:58:40 -0200

    adicionando includes e quebrando html em scripts php

---
+++ unidade4/layout/cabecalho.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Fundamentos para Internet - Estudo de Caso - Brasil</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="css/normalize.css">
+        <link rel="stylesheet" type="text/css" href="css/global.css">
+        <link rel="stylesheet" type="text/css" href="css/pagina-inicial.css">
+    </head>
+    <body>
+    <div class="tudo">
+        <div class="cabecalho">
+            <div class="cabecalho-informacao">
+                <div class="informacao-brasil">
+                    <img height="18" src="imagens/bandeira-brasil.png" alt="Bandeira do Brasil">
+                    <span>Brasil</span>
+                </div>
+                <div class="informacao-servicos">
+                    <ul>
+                        <li><a href="servico.html">Serviços</a></li>
+                    </ul>
+                </div>
+                <div class="informacao-links">
+                    <ul>
+                        <li><a href="participe.html">Participe</a></li>
+                        <li><a href="informacao.html">Acesso à Informação</a></li>
+                        <li><a href="legislacao.html">Legislação</a></li>
+                        <li><a href="canais.html">Canais</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="cabecalho-destaque">
+                <h1 class="titulo-portal">Portal Brasil</h1>
+                <div class="busca">
+                    <form>
+                        <input type="text" name="busca" class="input-busca" placeholder="buscar...">
+                        <input type="image" height="18" src="imagens/icone-busca.png">
+                    </form>
+                </div>
+            </div>
+            <div class="cabecalho-ouvidoria">
+                <ul>
+                    <li><a href="perguntas.html">Perguntas frequentes</a></li>
+                    <li><a href="fale-governo.html">Fale com o Governo</a></li>
+                    <li><a href="fale-presidente.html">Fale com o Presidente</a></li>
+                </ul>
+            </div>
+        </div>
