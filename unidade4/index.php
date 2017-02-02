0a5bc9576a3295d0259cd50b94af50288ade534f
Eduardo Bona <eduardobona@MacBook-Pro-de-Eduardo.local>
2017-02-01 22:55:35 -0200

    adicionando fotos para noticias e inserindo conteudo para trabalhar em seguida com arrays

--- unidade4/index.php
+++ unidade4/index.php
@@ -15,8 +15,43 @@
 <?php include "layout/estrutura_lateral.php";  ?>
 
             <div class="conteudo">
-                PREENCHER ESTE CONTEÚDO NA ATIVIDADE
-                DE DIAGRAMAÇÃO DA PÁGINA PRINCIPAL
+                <div class="noticia-destaque">
+                    <h3>Cidadania e Justiça</h3>
+                    <div class="noticia-chamada">
+                        <a href="#"><img src="imagens/noticias/destaque_bolsafamilia.jpeg" class="right"></a>
+                        <small>Políticas Públicas</small>
+                        <h4><a href="#">Governo federal zera fila de espera do Bolsa Família em janeiro</a></h4>
+                        <p><a href="#">Cerca de 460 mil pessoas que aguardavam para receber benefício foram incluídas no programa</a></p>
+                    </div>
+                </div>
+                <div class="noticia-lista">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                <img width="100%" src="imagens/noticias/mini_meirelles.jpeg">
+                            </a>
+                            <small>RECUPERAÇÃO ECONÔMICA</small>
+                            <h4><a href="#">Brasil sairá da recessão ainda no 1° trimestre, afirma ministro</a></h4>
+                            <p><a href="#">A investidores, Meirelles listou medidas para que País "volte a crescer com taxas mais elevadas"</a></p>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img width="100%" src="imagens/noticias/mini_exportacoes.jpeg">
+                            </a>
+                            <small>COMÉRCIO</small>
+                            <h4><a href="#">Balança de janeiro tem o maior superávit dos últimos 11 anos</a></h4>
+                            <p><a href="#">Exportações somaram US$ 14,91 bilhões e a importações chegaram a US$ 12,18 bilhões</a></p>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <img width="100%" src="imagens/noticias/mini_petrobras.jpeg" class="right">
+                            </a>
+                            <small>PETRÓLEO</small>
+                            <h4><a href="#">Ajuste de investimentos não reduzirá produção da Petrobras</a></h4>
+                            <p><a href="#">Presidente da estatal afirmou que empresa deve atingir meta sem prejudicar população</a></p>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
